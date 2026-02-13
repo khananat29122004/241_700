4d6f2def1da513c286fbc62a2431d540c5a483dd
khananat <khananat.j@ku.th>
2026-02-13 12:19:44 +0700

    Add files via upload

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,27 +1,62 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+app.use(bodyParser.json())
 
-function submitData () {
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]');
-    let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
-    let descriptionDOM = document.querySelector('textarea[name=description]');
+let users =[];
+let counter = 1;
 
-    let interest = ''
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value 
-        if (i != interestDOMs.length - 1) {
-            interest += ','
+//ต่อสัปดาหืหน้า
+
+
+app.get ('/users',(req,res)=>{
+    res.json(users);
+});
+//path post
+app.post ('/user',(req,res)=>{
+    let user = req.body;
+    users.push(user);
+    user.id = counter
+    counter +=1;
+    res.json({
+    message: 'User added sucess',
+    user:user
+    });
+});
+//path put
+app.put('/user/;id',(req,res)=>{
+    let id = res.params.id;
+    let updateUser = req.body;
+    // หา user จากid
+    let selectedIndex = users.findIndex(user => user.id == id);
+    res.send(selectedIndex+"")
+    //อัพเดทข้อมูลusers
+    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname;
+    users[selectedIndex].lastname = updateUser.lastnamename || users[selectedIndex].lastname;
+    res.json({
+        message:'User update success',
+        data:{
+             user:updateUser,
+             indexUpdate: selectedIndex
         }
-    }
+    });
+//if(user.id == id){
+  //          return true
+    //    }else{
+      //      return false
+        //}
+app.delete('/user/:id',(req,res)=>{
+    let id = req.params.id;
+     let selectedIndex = users.findIndex(user => user.id == id);
+     delete users[selectedIndex];
+     res.json({
+            message:'USer deleate success',
+            indexDelete: selectedIndex
+     });
+})
+})
 
-    let userData ={
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
-    }
-     console.log('submitData', userData);
-}
+app.listen(port,()=>{
+    console.log('Server runing on http://localhost:${port}');
+});
